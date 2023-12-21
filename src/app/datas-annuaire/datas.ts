f661ce157dc13055a6712f367e781de5636ffe0f
Sonia-Baibou <s.baibou@ohmycode.io>
2023-12-21 09:57:51 +0100

    ajout capacitor

--- src/app/datas-annuaire/datas.ts
+++ src/app/datas-annuaire/datas.ts
@@ -38,3 +38,43 @@ export const datasAnnuaire = [
     icon: 'basket',
   },
 ];
+
+
+export const datasDirectory = [
+  {
+    company: 'Restaurant Crêperie',
+    descr: 'restaurant breton',
+    cat: catEnum.RESTAURANT,
+    icon: 'restaurant',
+  },
+  {
+    company: 'Spa',
+    descr: 'salon de beauté',
+    cat: catEnum.BIENETRE,
+    icon: 'rose',
+  },
+  {
+    company: 'Hotel Maurice',
+    descr: 'hôtel ***',
+    cat: catEnum.HEBERGEMENT,
+    icon: 'home',
+  },
+  {
+    company: 'Hotel Ibis',
+    descr: 'hôtel **',
+    cat: catEnum.HEBERGEMENT,
+    icon: 'home',
+  },
+  {
+    company: 'Sport 2000',
+    descr: 'magasin de sport',
+    cat: catEnum.MAGASIN,
+    icon: 'basket',
+  },
+  {
+    company: 'Sport 3000',
+    descr: 'magasin de sport',
+    cat: catEnum.MAGASIN,
+    icon: 'basket',
+  },
+];
